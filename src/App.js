a00a101ae7a6a4773c4e83ec7b5aa6db91ae4837
FengDenny <fengdennyy@gmail.com>
2020-06-10 01:07:03 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 // import Home from './Routes/Home/Home'
-import Navbar from './Navbar/Navbar'
+import Navbar from './utility/Navbar/Navbar'
+import Home from './pages/Home/Home'
 
 function App() {
   return (
     <Router>
     <div className="App">
-      <Route exact path ="/" component={Navbar}/>
-      {/* <Route exact path ="/" component={Home}/> */}
+      <Route  exact path ="/" component={Navbar}/>
+      <Route  exact path ="/" component={Home}/>
     </div>
     </Router>
   );
