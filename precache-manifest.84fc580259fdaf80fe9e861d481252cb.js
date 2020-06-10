b24e869a07fa500cab80f35d52a960121f1ebb03
FengDenny <fengdennyy@gmail.com>
2020-06-10 00:38:01 -0700

    Updates

---
+++ precache-manifest.84fc580259fdaf80fe9e861d481252cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e591d913560ef630c39d81a404d60708",
+    "url": "/movies_db/index.html"
+  },
+  {
+    "revision": "5150cd9e7cc95e058028",
+    "url": "/movies_db/static/css/main.54889a01.chunk.css"
+  },
+  {
+    "revision": "7c9e18616e49240aea6b",
+    "url": "/movies_db/static/js/2.962b1b7e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies_db/static/js/2.962b1b7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5150cd9e7cc95e058028",
+    "url": "/movies_db/static/js/main.b5c2454a.chunk.js"
+  },
+  {
+    "revision": "cd654f73e938acc069f8",
+    "url": "/movies_db/static/js/runtime-main.c884b706.js"
+  }
+]);
