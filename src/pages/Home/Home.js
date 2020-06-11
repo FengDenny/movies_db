1ab6105549e50e02f375aa6dbb5efe47fda7f2d7
FengDenny <fengdennyy@gmail.com>
2020-06-10 18:08:10 -0700

    Get MBD API

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,63 @@
 import React, { Component } from 'react'
+import config from '../../config'
+import axios from 'axios'
+import {Link } from 'react-router-dom'
+import NowPlaying from '../NowPlaying/NowPlaying'
+import Theme from '../../img/Theme.jpg'
+import '../../css/Home.css'
 
  class Home extends Component {
+
+    state={
+        nowPlayingList:[],
+        nowPlayingMovies:{},
+        upcomingList:[],
+        upcomingMovies:{},
+        popularList:[],
+        popularityMovies:{}
+    }
+
+    async componentDidMount()
+    {
+        const nowPlayingUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${config.api_key}&language=en-US&page=1`
+        // const upcomingUrl = `https://api.themoviedb.org/3/movie/upcoming?api_key=${config.api_key}&language=en-US&page=1`
+        // const popularity = `https://api.themoviedb.org/3/movie/popular?api_key=${config.api_key}`
+
+        const moviePromises = [];
+               // send out all the api request at the same time using Promise.all
+            // data[0] = nowPlayingUrl
+
+        moviePromises.push(axios.get(nowPlayingUrl));
+        
+        Promise.all(moviePromises).then((data)=> {
+            const nowPlayingMovies = data[0].data.results;
+  
+            this.setState({
+                nowPlayingList: nowPlayingMovies,
+               
+            })
+        })
+
+
+    }
+
+
     render() {
+            // console.log(this.state.nowPlayingList)
+          
+
+
         return (
             <div>
-               <h1> Home</h1> 
+                <div className="hero">
+                    <div className="hero-content">
+                        <h1 className="heading-primary">MovieDB Re<span className="span">mix</span></h1>
+                        
+                    </div>
+                </div>
+               <div>
+                    <NowPlaying playing ={this.state.nowPlayingList} header="Now Playing" />
+               </div>
             </div>
         )
     }
