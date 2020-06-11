f844fbe26ba1171135c2c88258c1850fcdcab698
FengDenny <fengdennyy@gmail.com>
2020-06-10 18:08:29 -0700

    Create NowPlayings.js

---
+++ src/pages/NowPlaying/NowPlayings.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import {Link } from 'react-router-dom'
+
+class NowPlayings extends Component {
+    render() {
+
+        const imageUrl = "http://image.tmdb.org/t/p/w300"
+       const {poster_path, title,id} = this.props.nowPlaying
+
+        return (
+            <div>
+              <Link to={`/move/${id}`}>
+                   <img src={`${imageUrl}${poster_path}`} alt ="poster" />
+              </Link>
+              <div className="title">{title}</div>
+            </div>
+        )
+    }
+}
+
+export default  NowPlayings
