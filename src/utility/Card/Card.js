e899bf824608e856a194cb4738f6aa363e127510
FengDenny <fengdennyy@gmail.com>
2020-06-12 19:22:53 -0700

    Universal card component

---
+++ src/utility/Card/Card.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../../css/Card.css'
+
+ class Card extends Component {
+    render() {
+        return (
+            <div className="cards">
+                {this.props.elements}
+            </div>
+        )
+    }
+}
+
+export default Card
