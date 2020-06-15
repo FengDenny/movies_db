4244432063763d256f832f4c6235c18ad275767a
FengDenny <fengdennyy@gmail.com>
2020-06-15 14:06:12 -0700

    Prev/Next pages button

--- src/pages/TopRated/TopRated.js
+++ src/pages/TopRated/TopRated.js
@@ -4,52 +4,76 @@ import axios from 'axios'
 import '../../css/TopRated.css'
 import TopRates from './TopRates'
 import Card from '../../utility/Card/Card'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
  class  TopRated extends Component {
 
     state={
         TopRatedList:[],
-        TopRatedMovies:{}
+        TopRatedMovies:{}, 
+        pageNum :1,
+        totalPages:null
     }
 
         async componentDidMount()
         {
-            const topRatedURL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${config.api_key}&language=en-US&page=1&region=US`;
+            const topRatedURL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${config.api_key}&language=en-US&page=${this.state.pageNum}&region=US`;
             const ratedPromises =[];
 
             ratedPromises.push(axios.get(topRatedURL))
 
             Promise.all(ratedPromises).then((data)=>{
                 const TopRatedMovies = data[0].data.results;
+                const pages = data[0].data.total_pages;
 
                 this.setState({
-                    TopRatedList:TopRatedMovies
+                    TopRatedList:TopRatedMovies,
+                    totalPages: pages
                 })
-
-
             })
+        }
+      
+        nextPage = () => {
+            let page = this.state.pageNum
+            if(this.state.TopRatedList && this.state.pageNum < this.state.totalPages)
+            {
+                this.setState({
+                    pageNum: page += 1
+                },()=> this.componentDidMount() )
+            }
+        } 
 
+        prevPage = () => {
+            let page = this.state.pageNum
+            if(this.state.TopRatedList && this.state.pageNum !== 1)
+            {
+                this.setState({
+                    pageNum: page -= 1
+                },()=> this.componentDidMount() )
+            }
         }
 
      render()
+
      {
 
         const topRated = this.state.TopRatedList.map((top,i)=>{
             return(
-                <div key={i}>
+                <div className="border" key={i}>
                     <TopRates top ={top} key={i}/>
                 </div>
             )
         })
 
 
-        console.log(this.state.TopRatedList)
         return (
                 <div className="bg-color">
                 <div className="heading-primary heading-secondary">
                     <h1 className=" ">Top Rated</h1>
-                </div>
-                <Card elements={topRated}/>
+                </div>     
+                        <Card elements={topRated}/>            
+                     <button onClick={this.prevPage}>Previous Page</button>     
+                      <button onClick={this.nextPage}>Next Page</button>    
                 </div>
             )
      }
