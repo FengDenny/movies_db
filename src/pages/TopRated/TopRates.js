05bbbf808519c3651333f86a8cbf63c0ee079f7f
FengDenny <fengdennyy@gmail.com>
2020-06-15 15:22:43 -0700

    Popularity

--- src/pages/TopRated/TopRates.js
+++ src/pages/TopRated/TopRates.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
 import {Link } from 'react-router-dom'
+import '../../css/TopRated.css'
 
  class TopRates extends Component {
     render() {
 
         const imageUrl = "http://image.tmdb.org/t/p/w300"
-        const {poster_path, title,id} = this.props.top
+        const {poster_path, title,id, popularity} = this.props.top
+
 
 
         return (
-            <div>
+            <div className="card">
                <Link to={`/top_rated/${id}`}>
                    <img src={`${imageUrl}${poster_path}`} alt="Top" />
                </Link>
-               <div className="title">{title}</div>
+        <div className="card-title">{title}</div>
+        <div className="popularity">{popularity}%</div>
             </div>
         )
     }
