fe73cb318fb36721ce5e1e52162bdb3458a06700
FengDenny <fengdennyy@gmail.com>
2020-06-16 17:39:44 -0700

    Update NowPlayings.js

--- src/pages/NowPlaying/NowPlayings.js
+++ src/pages/NowPlaying/NowPlayings.js
@@ -6,14 +6,14 @@ class NowPlayings extends Component {
     render() {
 
         const imageUrl = "http://image.tmdb.org/t/p/w300"
-       const {poster_path, title,id} = this.props.nowPlaying
+       const {poster_path,id} = this.props.nowPlaying
 
         return (
             <div>
               <Link to={`/move/${id}`}>
                    <img src={`${imageUrl}${poster_path}`} alt ="poster" />
               </Link>
-              {/* <div className="title">{title}</div> */}
+              
             </div>
         )
     }
