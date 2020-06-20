ca03c7e519a9bc308f9af7839ad444ba8a0f9ced
FengDenny <fengdennyy@gmail.com>
2020-06-20 13:52:29 -0700

    Get on tv shows details

---
+++ src/routes/OnTV/OnTVDetails.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import config from '../../config'
+
+ class OnTVDetails extends Component {
+
+    state={
+        onTVDetails:{}
+    }
+
+        async componentDidMount(){
+            const onTVID = this.props.match.params.tvID
+            const singleTVURL =`https://api.themoviedb.org/3/tv/${onTVID}?api_key=${config.api_key}` 
+
+            axios.get(singleTVURL).then((res)=> {
+                this.setState({
+                    onTVDetails: res.data
+                })
+            })
+        }
+
+        back = () => {
+            window.history.back();
+        }
+
+
+
+    render() {
+
+        const onTVDetails = this.state.onTVDetails
+        const imageURL = `http://image.tmdb.org/t/p/w300${onTVDetails.poster_path}`;
+
+        return (
+            <div className="bg-color">
+                <div className="heading-primary heading-secondary">
+                       <h1>{onTVDetails.name}</h1> 
+                </div>
+                <div className="popularity popularity-overview">
+                    <p>Popularity: {onTVDetails.popularity}%</p>
+                </div>
+                <div className="image-width">
+                    <img src={imageURL} alt="on tv"/>
+                </div>
+                <div className="buttons back-btn"> 
+                     <button onClick={this.back}>Back</button>  
+                     </div>
+                <div className="p-overview">
+                    <p>{onTVDetails.overview}</p>
+                </div>
+                
+            </div>
+        )
+    }
+}
+export default OnTVDetails
