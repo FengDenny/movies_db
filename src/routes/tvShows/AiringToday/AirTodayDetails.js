d54e34e7374dbdf641d46d9e80e24fb20c7707aa
FengDenny <fengdennyy@gmail.com>
2020-06-20 13:53:05 -0700

    Get air today details

---
+++ src/routes/tvShows/AiringToday/AirTodayDetails.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+ class AirTodayDetails extends Component {
+    render() {
+        return (
+            <div className="bg-color">
+                <h1>Airing</h1>
+            </div>
+        )
+    }
+}
+export default AirTodayDetails
